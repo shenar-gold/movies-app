ea57d9d479653f860298a6c5dd478697504c600d
shynar <goldshynar@gmail.com>
2019-06-13 20:21:28 +0000

    Created dgango movie web app

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
-    path('create/', views.create, name='create')
+    path('create/', views.create, name='create'),
+    path('edit/<str:movie_id>', views.edit, name="edit"),
+    path('delete/<str:movie_id>', views.delete, name="delete"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -20,10 +20,43 @@ def create(request):
     if request.method == 'POST':
         data = {
             'Name': request.POST.get('name'),
+            'Pictures': [{'url': request.POST.get('url') or 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg'}],
+            'Rating': int(request.POST.get('rating')),
+            'Notes': request.POST.get('notes')
+        }
+        try:
+            response = AT.insert(data);
+            # notify on create
+            messages.success(request, "New Movie Added: {}".format(response['fields'].get('Name')));
+        except Exception as e:
+            messages.warning(request, "Got an error when trying to create a movie: {}".format(e));
+    return redirect('/');
+
+def edit(request, movie_id):
+    if request.method == 'POST':
+        data = {
+            'Name': request.POST.get('name'),
             'Pictures': [{'url': request.POST.get('url')}],
             'Rating': int(request.POST.get('rating')),
             'Notes': request.POST.get('notes')
         }
 
-        AT.insert(data)
+        try:
+            response = AT.update(movie_id, data);
+            #notify on update
+            messages.success(request, "Movie Updated: {}".format(response['fields'].get('Name')));
+        except Exception as e:
+            messages.warning(request, "Got an error when trying to update a movie: {}".format(e));
+
+    return redirect('/')
+
+def delete(request, movie_id):
+    try:
+        #print(movie_id);
+        movie_name = AT.get(movie_id)['fields']['Name'];
+        AT.delete(movie_id);
+        #notify on delete
+        messages.warning(request, "Deleted movie: {}".format(movie_name));
+    except Exception as e:
+        messages.warning(request, "Got an error when trying to delete a movie: {}".format(e));
     return redirect('/')
